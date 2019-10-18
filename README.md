b61691a7785409461d8366567832a514e659a638
nikkkkhil <thenikkkhil@yahoo.com>
2019-10-18 19:08:42 +0530

    added telegram notification

---
+++ README.md
@@ -0,0 +1,53 @@
+# WhatsApp online Checker 
+
+Want to spy your friend  on whatsapp ? then youre in the right place . This python script notifies you when someone comes online through Alert sound and get notified through telegram.
+  - instant online notifier
+  - with the time 
+  - telgram notification
+ 
+
+
+
+### Installation
+
+
+
+Install the dependencies and devDependencies .
+
+```sh
+$ git clone https://github.com/nikkkkhil/whatsapp_online_checker
+$ cd whatsapp_online_checker
+$ pip install -r requirements.txt 
+- create a telegram bot - check this link for createing bot and getting channelid and api  https://www.freecodecamp.org/news/learn-to-build-your-first-bot-in-telegram-with-python-4c99526765e4/
+-configure your api, channel id in code
+```
+```
+$ ./whatsapp.py
+```
+- firefox browser opens , scan your web whatsapp qr code from your mobile whatsapp and wait till the
+ page completely loads 
+
+- After successfully loading page enter the name of the person on console thay you want to spy on , name as to be exactly as a contact name saved in whatsapp atleast first word.
+You start seeing the whatsapp onine status of that person 
+```
+Enter a name of person : 
+```
+
+
+
+
+### Todos
+
+ - Somekind of desktop Notification 
+ 
+
+License
+----
+
+
+
+
+**Free Software, Hell Yeah!**
+
+
+
--- whatsapp.py
+++ whatsapp.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import pygame
 import os
 from datetime import datetime
+import requests
 class WhatsappOnline:
     status = None
     search_bar_selector = '#side > div._2HS9r > div > label > input'
@@ -15,6 +16,11 @@ class WhatsappOnline:
         self.browser = webdriver.Firefox(executable_path = './bin/geckodriver', log_path = './bin/geckodriver.log')
         self.browser.get('https://web.whatsapp.com')
         print("\nWelcome to the whatsapp bot\n")
+
+    def telegram_bot(self,bot_token,bot_chatID,bot_message):
+        send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+        response = requests.get(send_text)
+        return response.json()
 
     def online_check(self,name):
         search_bar = self.browser.find_element_by_css_selector(
@@ -34,6 +40,8 @@ class WhatsappOnline:
                 self.status = 'online'
                 if count ==1:
                     get_time = datetime.now().strftime("%H:%M:%S:%D")
+                    msg = name + " is online at time "+ get_time
+                    self.telegram_bot(<telegramAPI>,<telegramchannelID>,msg)
                     print(get_time)
                     pygame.mixer.music.play()
             else:
